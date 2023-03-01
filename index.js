bc16f710bffbacdcff88125c49fd6512501d80e3
Washiner Takeuchi <washiner@gmail.com>
2023-03-01 07:00:19 -0400

    aula de modules node

---
+++ index.js
@@ -0,0 +1,16 @@
+let SomaFunc = require("./somar")
+let SubFunc = require("./subtrair")
+let MultFunc = require("./multiplicar")
+let DivFunc = require("./dividir")
+
+console.log(
+    SomaFunc(100,100)
+)
+
+console.log(
+    SubFunc(300,150)
+)
+
+console.log(MultFunc(10,50))
+
+console.log(DivFunc(100,3))
---
+++ multiplicar.js
@@ -0,0 +1,5 @@
+let mult = function(a,b){
+    return a*b
+}
+
+module.exports = mult
